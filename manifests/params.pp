e07a04f15d3ed1ca93985c2dd4c8eb9cb0bd5c5f
Schonecker, Brian <bschonecker@healthplan.com>
2016-04-19 13:41:09 -0400

    Removed WGET installation in favor of yum (or local package manager) install.  Also added some arguments to help with installation and setup.

--- manifests/params.pp
+++ manifests/params.pp
@@ -19,28 +19,4 @@
 # Copyright 2014 Timothy Scoppetta, unless otherwise noted.
 
 class bigfix::params{
-  if $bigfix::client::version == '9.1'{
-    case $::operatingsystem {
-      centos, redhat: {
-        $agent_url='http://software.bigfix.com/download/bes/91/BESAgent-9.1.1082.0-rhe5.x86_64.rpm'
-        $server_url='http://software.bigfix.com/download/bes/91/ServerInstaller_9.1.1082.0-rhe6.x86_64.tgz'
-        $relay_url='http://software.bigfix.com/download/bes/91/BESRelay-9.1.1082.0-rhe5.x86_64.rpm'
-      }
-      debian, ubuntu: {
-        $agent_url='http://software.bigfix.com/download/bes/91/BESAgent-9.1.1082.0-ubuntu10.amd64.deb'
-      }
-    }
-    } elsif $bigfix::client::version == '9.0' {
-    case $::operatingsystem {
-      centos, redhat: {
-        $agent_url='http://software.bigfix.com/download/bes/90/BESAgent-9.0.787.0-rhe5.x86_64.rpm'
-        $relay_url='http://software.bigfix.com/download/bes/90/BESRelay-9.0.787.0-rhe5.x86_64.rpm'
-      }
-      debian, ubuntu: {
-        $agent_url='http://software.bigfix.com/download/bes/90/BESAgent-9.0.787.0-ubuntu10.amd64.deb'
-      }
-    }
-    } else {
-      fail('Unsupported version')
-    }
 }
